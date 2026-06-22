d821b3dff9a753782cfa43f9caca32964bc4382d
Soar Tyler (2023) ZMAC509 <tyler.soar.2023@live.rhul.ac.uk>
2026-06-22 15:11:07 +0100

    added get_q_value function that returns a filled q table in current state. added choose_action function which randomly explores and chooses best action

--- QLearningAgent.py
+++ QLearningAgent.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class QLearningAgent:
     def __init__(self, n_actions, gamma, alpha, epsilon, epsilon_decay, epsilon_min):
         self.n_actions = n_actions
@@ -10,3 +12,20 @@ class QLearningAgent:
         # a dictionary that maps tuples -> action values
         # if a state is new 0s are returned
         self.q_table = []
+
+    def get_q_values(self, state_key):
+        if state_key not in self.q_table:
+            self.q_table[state_key] = np.zeros(self.n_actions)
+        return self.q_table[state_key]
+
+    def choose_action(self, state_key):
+        # Exploration greedy
+        if np.random.random() < self.epsilon:
+            return np.random.randint(0,self.n_actions)
+
+        # gets the best known value (exploitation)
+        q_values = self.get_q_values(state_key)
+        # randomly choose among the best if there is a tie (prevents from getting stuck)
+        max_q = np.max(q_values)
+        best_actions = np.where(q_values == max_q)[0]
+        return np.random.choice(best_actions)
